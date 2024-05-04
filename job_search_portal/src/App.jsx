f50c25672e41aefda2a9de572296be38a831f038
AshishBBG <abhisingh789123@gmail.com>
2024-05-04 15:32:09 +0530

    html structure of web page is ready

--- job_search_portal/src/App.jsx
+++ job_search_portal/src/App.jsx
@@ -5,7 +5,186 @@ function App() {
 
   return (
    <>
-   <h1>Hello, world!</h1>
+       {/* <!-- top section starts --> */}
+    <div className="main-container">
+        <input type="text" placeholder="Roles" className="input-box-job" />
+        <input type="text" placeholder="Number Of Employees" className="input-box-job" />
+        <input type="text" placeholder="Experience" className="input-box-job" />
+        <input type="text" placeholder="Remote" className="input-box-job" />
+        <input type="text" placeholder="Minimum Base Pay salary" className="input-box-job" />
+        <input type="text" placeholder="Search Company Name" className="input-box-job" />
+    </div>
+    {/* <!-- top section ends here --> */}
+    <div className="job-container">
+
+        {/* <!-- job 1 container start here --> */}
+        <div className="job-container-padding">
+            <div className="posted-date-container">
+                <span className="posted-date">⏳ Posted 3 days ago</span>
+            </div>
+            <div className="company-info">
+                <div className="left-side-logo">
+                    <img src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713846233282_m137p.jpg"
+                        alt="" />
+                </div>
+                <div className="company-info-post">
+                    <p className="company-name">Firefly</p>
+                    <p>Frontend Engineer</p>
+                    <p className="company-location">India</p>
+                </div>
+            </div>
+            <div className="company-salary">
+                <p>Estimated Salary: ₹30 - 50 LPA</p>
+                <p>✅</p>
+            </div>
+
+            <div className="company-para">
+                <p className="about-company">About Company:</p>
+                <p className="about-us">About us</p>
+                <p className="about-company-para fade">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est saepe
+                    deleniti excepturi placeat dignissimos nam illum, nesciunt facilis autem quibusdam, voluptatibus
+                    minus distinctio qui quidem tempora. Ipsam repudiandae totam porro.</p>
+            </div>
+            <div className="job-link">
+                <a href="#">View Job</a>
+            </div>
+
+            <div className="job-experience">
+                <p  className="min-exp">Minimum Experience</p>
+                <p>4 years</p>
+            </div>
+
+            <button  className="apply-btn">⚡ Easy Apply</button>
+        </div>
+        {/* <!-- job 1 container end here --> */}
+
+        
+        {/* <!-- job 2 container start here --> */}
+        <div className="job-container-padding">
+            <div className="posted-date-container">
+                <span className="posted-date">⏳ Posted 3 days ago</span>
+            </div>
+            <div className="company-info">
+                <div className="left-side-logo">
+                    <img src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713846233282_m137p.jpg"
+                        alt="" />
+                </div>
+                <div className="company-info-post">
+                    <p className="company-name">Firefly</p>
+                    <p>Frontend Engineer</p>
+                    <p className="company-location">India</p>
+                </div>
+            </div>
+            <div className="company-salary">
+                <p>Estimated Salary: ₹30 - 50 LPA</p>
+                <p>✅</p>
+            </div>
+
+            <div className="company-para">
+                <p className="about-company">About Company:</p>
+                <p className="about-us">About us</p>
+                <p className="about-company-para fade">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est saepe
+                    deleniti excepturi placeat dignissimos nam illum, nesciunt facilis autem quibusdam, voluptatibus
+                    minus distinctio qui quidem tempora. Ipsam repudiandae totam porro.</p>
+            </div>
+            <div className="job-link">
+                <a href="#">View Job</a>
+            </div>
+
+            <div className="job-experience">
+                <p  className="min-exp">Minimum Experience</p>
+                <p>4 years</p>
+            </div>
+
+            <button  className="apply-btn">⚡ Easy Apply</button>
+        </div>
+        {/* <!-- job 2 container end here --> */}
+
+            
+        {/* <!-- job 3 container start here --> */}
+        <div className="job-container-padding">
+            <div className="posted-date-container">
+                <span className="posted-date">⏳ Posted 3 days ago</span>
+            </div>
+            <div className="company-info">
+                <div className="left-side-logo">
+                    <img src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713846233282_m137p.jpg"
+                        alt="" />
+                </div>
+                <div className="company-info-post">
+                    <p className="company-name">Firefly</p>
+                    <p>Frontend Engineer</p>
+                    <p className="company-location">India</p>
+                </div>
+            </div>
+            <div className="company-salary">
+                <p>Estimated Salary: ₹30 - 50 LPA</p>
+                <p>✅</p>
+            </div>
+
+            <div className="company-para">
+                <p className="about-company">About Company:</p>
+                <p className="about-us">About us</p>
+                <p className="about-company-para fade">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est saepe
+                    deleniti excepturi placeat dignissimos nam illum, nesciunt facilis autem quibusdam, voluptatibus
+                    minus distinctio qui quidem tempora. Ipsam repudiandae totam porro.</p>
+            </div>
+            <div className="job-link">
+                <a href="#">View Job</a>
+            </div>
+
+            <div className="job-experience">
+                <p  className="min-exp">Minimum Experience</p>
+                <p>4 years</p>
+            </div>
+
+            <button  className="apply-btn">⚡ Easy Apply</button>
+        </div>
+        {/* <!-- job 3 container end here --> */}
+
+            
+        {/* <!-- job 4 container start here --> */}
+        <div className="job-container-padding">
+            <div className="posted-date-container">
+                <span className="posted-date">⏳ Posted 3 days ago</span>
+            </div>
+            <div className="company-info">
+                <div className="left-side-logo">
+                    <img src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713846233282_m137p.jpg"
+                        alt="" />
+                </div>
+                <div className="company-info-post">
+                    <p className="company-name">Firefly</p>
+                    <p>Frontend Engineer</p>
+                    <p className="company-location">India</p>
+                </div>
+            </div>
+            <div className="company-salary">
+                <p>Estimated Salary: ₹30 - 50 LPA</p>
+                <p>✅</p>
+            </div>
+
+            <div className="company-para">
+                <p className="about-company">About Company:</p>
+                <p className="about-us">About us</p>
+                <p className="about-company-para fade">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est saepe
+                    deleniti excepturi placeat dignissimos nam illum, nesciunt facilis autem quibusdam, voluptatibus
+                    minus distinctio qui quidem tempora. Ipsam repudiandae totam porro.</p>
+            </div>
+            <div className="job-link">
+                <a href="#">View Job</a>
+            </div>
+
+            <div className="job-experience">
+                <p  className="min-exp">Minimum Experience</p>
+                <p>4 years</p>
+            </div>
+
+            <button  className="apply-btn">⚡ Easy Apply</button>
+        </div>
+        {/* <!-- job 4 container end here --> */}
+
+    </div>
    </>
   )
 }
